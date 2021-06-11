1a12dd7a5a081637351be3259af41fa35331d202
Mateus De Nardo <mateusdnm@hotmail.com>
2021-06-11 18:52:18 -0300

    Desafio 02

--- data.js
+++ data.js
@@ -3,143 +3,87 @@ const BASE_PATH = 'https://image.tmdb.org/t/p/original/';
 const Movies = [
   {
     title: 'Mortal Kombat',
-    backgroundImg: `${BASE_PATH}w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg`,
-    isStarred: false,
-    starsCount: 9,
-    price: 7.5,
-    categories: [
-      'horror', 'action'
-    ]
+    poster_path: `${BASE_PATH}w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg`,
+    vote_average: 9,
+    price: 7.5
   },
   {
     title: 'Godzilla vs. Kong',
-    backgroundImg: `${BASE_PATH}klAIFewuqcyEmH1SMtR2XbMyqzM.jpg`,
-    isStarred: false,
-    starsCount: 8.5,
-    price: 3,
-    categories: [
-      'science fiction', 'action'
-    ]
+    poster_path: `${BASE_PATH}klAIFewuqcyEmH1SMtR2XbMyqzM.jpg`,
+    vote_average: 8.5,
+    price: 3
   },
   {
     title: 'Anônimo',
-    backgroundImg: `${BASE_PATH}woPYFGKwVULILbO7q44HmsNY0kO.jpg`,
-    isStarred: false,
-    starsCount: 8.5,
-    price: 7,
-    categories: [
-      'horror', 'action'
-    ]
+    poster_path: `${BASE_PATH}woPYFGKwVULILbO7q44HmsNY0kO.jpg`,
+    vote_average: 8.5,
+    price: 7
   },
   {
     title: 'Sem Remorso',
-    backgroundImg: `${BASE_PATH}uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg`,
-    isStarred: false,
-    starsCount: 8.4,
-    price: 5.5,
-    categories: [
-      'action'
-    ]
+    poster_path: `${BASE_PATH}uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg`,
+    vote_average: 8.4,
+    price: 5.5
   },
   {
     title: 'Demon Slayer: O Trem Infinito',
-    backgroundImg: `${BASE_PATH}m9cn5mhW519QKr1YGpGxNWi98VJ.jpg`,
-    isStarred: false,
-    starsCount: 8.4,
-    price: 8,
-    categories: [
-      'action'
-    ]
+    poster_path: `${BASE_PATH}m9cn5mhW519QKr1YGpGxNWi98VJ.jpg`,
+    vote_average: 8.4,
+    price: 8
   },
   {
     title: 'Cherry: Inocência Perdida',
-    backgroundImg: `${BASE_PATH}qbSsR0IfnxdXZqfRMpH2PgLAbPV.jpg`,
-    isStarred: false,
-    starsCount: 8.1,
-    price: 8.5,
-    categories: [
-      'romance'
-    ]
+    poster_path: `${BASE_PATH}qbSsR0IfnxdXZqfRMpH2PgLAbPV.jpg`,
+    vote_average: 8.1,
+    price: 8.5
   },
   {
     title: 'Benny Loves You',
-    backgroundImg: `${BASE_PATH}mQ8vALvqA0z0qglG3gI6xpVcslo.jpg`,
-    isStarred: false,
-    starsCount: 7.7,
-    price: 6.5,
-    categories: [
-      'horror'
-    ]
+    poster_path: `${BASE_PATH}mQ8vALvqA0z0qglG3gI6xpVcslo.jpg`,
+    vote_average: 7.7,
+    price: 6.5
   },
   {
     title: 'Mortal Kombat',
-    backgroundImg: `${BASE_PATH}w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg`,
-    isStarred: false,
-    starsCount: 9,
-    price: 7.5,
-    categories: [
-      'horror', 'action'
-    ]
+    poster_path: `${BASE_PATH}w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg`,
+    vote_average: 9,
+    price: 7.5
   },
   {
     title: 'Godzilla vs. Kong',
-    backgroundImg: `${BASE_PATH}klAIFewuqcyEmH1SMtR2XbMyqzM.jpg`,
-    isStarred: false,
-    starsCount: 8.5,
-    price: 3,
-    categories: [
-      'science fiction', 'action'
-    ]
+    poster_path: `${BASE_PATH}klAIFewuqcyEmH1SMtR2XbMyqzM.jpg`,
+    vote_average: 8.5,
+    price: 3
   },
   {
     title: 'Anônimo',
-    backgroundImg: `${BASE_PATH}woPYFGKwVULILbO7q44HmsNY0kO.jpg`,
-    isStarred: false,
-    starsCount: 8.5,
-    price: 7,
-    categories: [
-      'horror', 'action'
-    ]
+    poster_path: `${BASE_PATH}woPYFGKwVULILbO7q44HmsNY0kO.jpg`,
+    vote_average: 8.5,
+    price: 7
   },
   {
     title: 'Sem Remorso',
-    backgroundImg: `${BASE_PATH}uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg`,
-    isStarred: false,
-    starsCount: 8.4,
-    price: 5.5,
-    categories: [
-      'action'
-    ]
+    poster_path: `${BASE_PATH}uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg`,
+    vote_average: 8.4,
+    price: 5.5
   },
   {
     title: 'Demon Slayer: O Trem Infinito',
-    backgroundImg: `${BASE_PATH}m9cn5mhW519QKr1YGpGxNWi98VJ.jpg`,
-    isStarred: false,
-    starsCount: 8.4,
-    price: 8,
-    categories: [
-      'action'
-    ]
+    poster_path: `${BASE_PATH}m9cn5mhW519QKr1YGpGxNWi98VJ.jpg`,
+    vote_average: 8.4,
+    price: 8
   },
   {
     title: 'Cherry: Inocência Perdida',
-    backgroundImg: `${BASE_PATH}qbSsR0IfnxdXZqfRMpH2PgLAbPV.jpg`,
-    isStarred: false,
-    starsCount: 8.1,
-    price: 8.5,
-    categories: [
-      'romance'
-    ]
+    poster_path: `${BASE_PATH}qbSsR0IfnxdXZqfRMpH2PgLAbPV.jpg`,
+    vote_average: 8.1,
+    price: 8.5
   },
   {
     title: 'Benny Loves You',
-    backgroundImg: `${BASE_PATH}mQ8vALvqA0z0qglG3gI6xpVcslo.jpg`,
-    isStarred: false,
-    starsCount: 7.7,
-    price: 6.5,
-    categories: [
-      'horror'
-    ]
+    poster_path: `${BASE_PATH}mQ8vALvqA0z0qglG3gI6xpVcslo.jpg`,
+    vote_average: 7.7,
+    price: 6.5
   },
 ]
 
